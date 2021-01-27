ab642e6a7b4e680dc92aae49fe96010b3ea0455d
Shun Shibata <s.shibata@unisystem.co.jp>
2021-01-27 18:14:51 +0900

    settings.pyの分割対応

--- src/USWEB/settings/base.py
+++ src/USWEB/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@@s^_s56i*1!lhnyvs6i*%m&19n=+4%o=4_7_8!t+vu!)rfnaa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# 個別設定化
+#DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -76,17 +77,17 @@ WSGI_APPLICATION = 'USWEB.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# 個別設定化
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
